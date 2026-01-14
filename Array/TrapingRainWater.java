110b1b641116327aea2ab19e920654051ff2b288
adityasolankiii <adityasolanki845@gmail.com>
2026-01-14 22:03:22 +0530

    Trapping rain water problem

--- Array/TrapingRainWater.java
+++ Array/TrapingRainWater.java
@@ -1,30 +1,32 @@
-class TrapWater{
+package Array;
+
+class TrappingRainWater{
     
-    public static int trappedRainWater(int height[]) {
+    public static int trappedRainWater(int[] height) {
 
     int n = height.length;
-    //Calculate left max boundry -- Auxilary Array (helper array)
-    int leftMax[]=new int[n];
-    leftMax[0]=height[0];
+    //Calculate left max boundary -- Auxiliary Array (helper array)
+    int[] leftMax = new int[n];
+    leftMax[0] = height[0];
 
     for (int i = 1; i < n; i++) {
-        leftMax[i]=Math.max(height[i],leftMax[i-1]);
+        leftMax[i] = Math.max(height[i],leftMax[i-1]);
     }
 
-    //Calculate right max boundry -- Auxilary Array (helper array) 
-    int rightMax[]=new int[n];
-    rightMax[n-1]=height[n-1];
-    for(int i=n-2;i>=0;i--){
-        rightMax[i]=Math.max(height[i],rightMax[i+1]);
+    //Calculate right max boundary -- Auxiliary Array (helper array)
+    int[] rightMax = new int[n];
+    rightMax[n-1] = height[n-1];
+    for(int i = n-2; i >= 0; i--){
+        rightMax[i] = Math.max(height[i], rightMax[i+1]);
     }
 
-    int trappedWater=0;
+    int trappedWater = 0;
     //loop
-    for(int i=0;i<n;i++){
-        //waterLevel = min(leftMax Boundry,rightMax Boundry)
-        int waterLevel = Math.min(leftMax[i],rightMax[i]);
+    for(int i = 0; i < n; i++){
+        //waterLevel = min(leftMax Boundary, rightMax Boundary)
+        int waterLevel = Math.min(leftMax[i], rightMax[i]);
 
-        //trapped water = waterLevel - hight[i]
+        //trapped water = waterLevel - height[i]
         trappedWater += waterLevel - height[i];
     }
     
