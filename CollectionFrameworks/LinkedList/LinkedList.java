ee23bcc4353e7c31a5859a84274f2410cd037279
adityasolankiii <adityasolanki845@gmail.com>
2026-02-14 11:11:28 +0530

    LinkedList addFirst, addLast, print method

--- CollectionFrameworks/LinkedList/LinkedList.java
+++ CollectionFrameworks/LinkedList/LinkedList.java
@@ -1,6 +1,6 @@
 package CollectionFrameworks.LinkedList;
 
-public class LinkedListPractices {
+public class LinkedList {
 
     public static class Node {
         int data;
@@ -10,27 +10,67 @@ public class LinkedListPractices {
             this.data = data;
             this.next = null;
         }
+    }
 
-        public static Node head;
-        public static Node tail;
+    public static Node head;
+    public static Node tail;
 
-        private void addFirst(int data){
-            //Step-1 : create new node
-            Node newNode = new Node(data);
-            if(head == null){
-                head = tail = newNode;
-                return;
-            }
-
-            //Step-2 : newNode.next = head
-            newNode.next = head;
-
-            //Step-3 : head = newNode
-            head = newNode;
+    /***
+     * This method will add data at first in linked list
+     * @param data int
+     */
+    private void addFirst(int data){
+        //Step-1 : create new node
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
         }
+        //Step-2 : newNode.next = head
+        newNode.next = head;
+
+        //Step-3 : head = newNode
+        head = newNode;
+    }
+
+    /***
+     * This method will add data at last in linked list
+     * @param data int
+     */
+    private void addLast(int data){
+        //Step-1 : create new node
+        Node newNode = new Node(data);
+
+        if(tail == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //Step-2 : tail.next = newNode
+        tail.next = newNode;
+
+        //Step-3 : newNode = tail
+        tail = newNode;
+    }
+
+    private void print(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        Li
+        LinkedList ll = new LinkedList();
+
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+
+
+        ll.print();
     }
 }
