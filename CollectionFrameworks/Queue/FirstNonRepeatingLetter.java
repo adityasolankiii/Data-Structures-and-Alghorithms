a9319c4d87b613c5f298dd347524f0f87eb49aa4
adityasolankiii <adityasolanki845@gmail.com>
2026-02-28 19:12:27 +0530

    Queue: first non-repeating character

--- CollectionFrameworks/Queue/FirstNonRepeatingLetter.java
+++ CollectionFrameworks/Queue/FirstNonRepeatingLetter.java
@@ -1,4 +1,37 @@
 package CollectionFrameworks.Queue;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class FirstNonRepeatingLetter {
+
+    private static char find(String str){
+        Queue<Character> q = new ArrayDeque<>();
+        int[] freq = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int idx = c - 'a';
+            if(freq[idx] < 2){
+                q.add(c);
+            }
+            freq[idx] += 1;
+        }
+
+        while (!q.isEmpty()) {
+            char c = q.remove();
+            if (freq[c - 'a'] == 1) {
+                return c;
+            }
+        }
+        return '0';
+    }
+
+    public static void main(String[] args){
+        System.out.print("Enter String : ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("First non repeating character : " + find(input));
+    }
 }
