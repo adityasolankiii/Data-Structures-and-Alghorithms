c5d43c77e25809356d2706e314b5e2b3d4b56286
adityasolankiii <adityasolanki845@gmail.com>
2026-03-01 10:00:48 +0530

    Queue:  First non-repeating letter

--- CollectionFrameworks/Queue/FirstNonRepeatingLetter.java
+++ CollectionFrameworks/Queue/FirstNonRepeatingLetter.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class FirstNonRepeatingLetter {
 
-    private static char find(String str){
+    private static char find(String str) {
         Queue<Character> q = new ArrayDeque<>();
         int[] freq = new int[26];
 
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
             int idx = c - 'a';
-            if(freq[idx] < 2){
+            if (freq[idx] < 2) {
                 q.add(c);
             }
             freq[idx] += 1;
@@ -28,7 +28,7 @@ public class FirstNonRepeatingLetter {
         return '0';
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Enter String : ");
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
