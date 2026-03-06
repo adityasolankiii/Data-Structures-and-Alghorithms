b49b655d99052bae110f20e1a2393a8b477d2b92
adityasolankiii <adityasolanki845@gmail.com>
2026-03-06 19:44:29 +0530

    Greedy: Fractional knapsack problem

---
+++ GreedyAlgorithms/FractionalKnapsack.java
@@ -0,0 +1,42 @@
+package GreedyAlgorithms;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FractionalKnapsack {
+
+    public static void main(String[] args) {
+        int[] value = {120,100,60};
+        int[] weight = {30,20,10};
+
+        int w = 50;
+
+        double[][] ratio = new double[value.length][2];
+
+        for (int i = 0; i < ratio.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = (double) value[i] /weight[i];
+        }
+
+        Arrays.sort(ratio, Comparator.comparingDouble((double[] a)->a[1]).reversed());
+
+        int capacity  = w;
+        double finalVal = 0;
+
+        for (int i = 0; i < ratio.length; i++) {
+            int idx =(int) ratio[i][0];
+
+            if(capacity >= weight[idx]){ //include full item
+                finalVal+=value[idx];
+                capacity-=weight[idx];
+            } else { //include fractional item
+                finalVal += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
+
+        System.out.println("Final Value: "+finalVal);
+    }
+
+}
