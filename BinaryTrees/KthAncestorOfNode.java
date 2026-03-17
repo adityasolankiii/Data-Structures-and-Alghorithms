f0085d4d70d5a0f74180ea2ce02613a73efcc56b
adityasolankiii <adityasolanki845@gmail.com>
2026-03-17 16:29:51 +0530

    BinaryTree: Kth ancestor of the node

---
+++ BinaryTrees/KthAncestorOfNode.java
@@ -0,0 +1,33 @@
+package BinaryTrees;
+
+public class KthAncestorOfNode {
+
+    public static int findKthAncestor(Node root, int n, int k) {
+        if(root == null) return -1;
+
+        if(root.data == n) return 0;
+
+        int leftDist = findKthAncestor(root.left, n, k);
+        int rightDist = findKthAncestor(root.right, n, k);
+
+        if(leftDist == -1 && rightDist == -1) return -1;
+
+        int max = Math.max(leftDist, rightDist);
+
+        if(max+1 == k) System.out.println(root.data);
+
+        return max+1;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        findKthAncestor(root, 4, 2);
+    }
+}
