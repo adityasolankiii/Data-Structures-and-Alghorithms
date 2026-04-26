e5b584a3db63c5018c5368fd567896d1639cb432
adityasolankiii <adityasolanki845@gmail.com>
2026-04-26 22:45:43 +0530

    Hashing: sub array sum equal to k

--- Hashing/SubArraySumEqualToK.java
+++ Hashing/SubArraySumEqualToK.java
@@ -1,4 +1,28 @@
 package Hashing;
 
+import java.util.HashMap;
+
 public class SubArraySumEqualToK {
+    public static void main(String[] args) {
+        int[] arr = {10, -2, 2, -8, 1, 7, 10};
+        int k = 10;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //(sum, count)
+        map.put(0,1);
+
+        int sum = 0;
+        int ans = 0;
+
+        for(int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            if(map.containsKey(sum-k)) {
+                ans += map.get(sum-k);
+            }
+
+            map.put(sum, map.getOrDefault(sum,0) + 1);
+        }
+
+        System.out.println(ans);
+    }
 }
