23fc835fb31b722c76c26f62ed842ef868ea181e
adityasolankiii <adityasolanki845@gmail.com>
2026-05-02 23:35:03 +0530

    Tries: Creating a Trie

--- Tries/TrieImplementation.java
+++ Tries/TrieImplementation.java
@@ -1,41 +1,11 @@
-package tries;
+package Tries;
 
-public class Trie {
-
-    public static class Node {
-        Node[] children = new Node[26];
-        boolean endOfWord = false;
-
-        public Node() {
-            for (int i = 0; i < 26; i++) {
-                children[i] = null;
-            }
-        }
-    }
-
-    public static Node root = new Node();
-
-    public void insert(String word) {
-        Node curr = root;
-
-        for(int level = 0; level<word.length(); level++) {
-            int idx = word.charAt(level) - 'a';
-
-            if(curr.children[idx] == null) {
-                curr.children[idx] = new Node();
-            }
-            curr = curr.children[idx];
-        }
-
-        curr.endOfWord = true;
-    }
-
+public class TrieImplementation {
     public static void main(String[] args) {
         String[] words = {"the", "a", "there", "their", "any", "thee"};
 
-        Trie trie = new Trie();
         for(String word : words) {
-            trie.insert(word);
+            Trie.insert(word);
         }
     }
 }
